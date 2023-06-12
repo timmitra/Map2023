8cafcebe6e6d20479b724147c24d938179d74d4e
Tim Mitra <tim@it-guy.com>
2023-06-11 21:54:29 -0400

    Initial Commit

---
+++ Map2023/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Map2023
+//
+//  Created by Tim Mitra on 2023-06-11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Map2023/Map2023App.swift
@@ -0,0 +1,17 @@
+//
+//  Map2023App.swift
+//  Map2023
+//
+//  Created by Tim Mitra on 2023-06-11.
+//
+
+import SwiftUI
+
+@main
+struct Map2023App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
